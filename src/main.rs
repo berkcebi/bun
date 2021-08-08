9971054e50882711bd9fbe74e0161d8e6d9d7e45
Berk Çebi <berk@zeplin.io>
2021-08-08 02:40:04 -0700

    Use ability

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,17 @@ fn main() {
                 .with_run_criteria(bevy::core::FixedTimestep::step(1.0))
                 .with_system(regen_mana.system()),
         )
+        .add_system(handle_keyboard_input.system())
+        .add_system(use_ability.system())
         .run();
 }
+
+struct Ability {
+    name: String,
+    mana_points: u8,
+}
+
+struct Player;
 
 struct Mana {
     points: u8,
@@ -30,12 +39,33 @@ struct Mana {
     regen_points: u8,
 }
 
+struct UseAbility {
+    ability: Ability,
+}
+
 fn setup(mut commands: Commands) {
-    commands.spawn().insert(Mana {
+    commands.spawn().insert(Player).insert(Mana {
         points: 50,
         max_points: 100,
         regen_points: 3,
     });
+}
+
+fn handle_keyboard_input(
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+    player_query: Query<Entity, With<Player>>,
+) {
+    let player_entity = player_query.single().unwrap();
+
+    if keyboard_input.just_pressed(KeyCode::Key1) {
+        commands.entity(player_entity).insert(UseAbility {
+            ability: Ability {
+                name: "Fireball".to_string(),
+                mana_points: 25,
+            },
+        });
+    }
 }
 
 fn regen_mana(mut query: Query<&mut Mana>) {
@@ -45,5 +75,18 @@ fn regen_mana(mut query: Query<&mut Mana>) {
         }
 
         println!("{} / {}", mana.points, mana.max_points);
+    }
+}
+
+fn use_ability(mut commands: Commands, mut query: Query<(Entity, &UseAbility, &mut Mana)>) {
+    for (entity, use_ability_intent, mut mana) in query.iter_mut() {
+        if use_ability_intent.ability.mana_points < mana.points {
+            mana.points -= use_ability_intent.ability.mana_points;
+            println!("Casting {}!", use_ability_intent.ability.name);
+        } else {
+            println!("Not enough mana…");
+        }
+
+        commands.entity(entity).remove::<UseAbility>();
     }
 }
